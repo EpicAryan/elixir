a2161114efbf4c90cc9fc54a6c241fb2cdd97e28
EpicAryan <adaryan0811@gmail.com>
2025-06-18 17:05:49 +0530

    feat: hero section complete

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,16 +1,6 @@
-// import React from 'react'
-
-// const HeroSection = () => {
-//   return (
-//     <div>HeroSection</div>
-//   )
-// }
-
-// export default HeroSection
-
 "use client";
 import Image from 'next/image';
-import { motion, easeOut  } from 'motion/react';
+import { motion, easeOut } from 'motion/react';
 import { Button } from './ui/button';
 import { Star } from 'lucide-react';
 
@@ -60,26 +50,27 @@ const Hero = () => {
                 ease: easeOut,
             },
         },
+        hover: { scale: 1.05 },
     };
 
   
     const partners = [
-    { src: '/partner/casagrand.png', alt: 'Casagrand', width: 110, height: 40 },
-    { src: '/partner/sis-logo.png', alt: 'SIS', width: 60, height: 20 },
-    { src: '/partner/vgn.png', alt: 'VGN', width: 90, height: 35 },
-    { src: '/partner/godrej.png', alt: 'Godrej', width: 70, height: 40 },
-    { src: '/partner/roca.png', alt: 'Roca', width: 68, height: 30 },
-    { src: '/partner/puravankara.png', alt: 'Puravankara', width: 110, height: 35 },
-    { src: '/partner/greenply.png', alt: 'Greenply', width: 60, height: 40 },
+      { src: '/partner/casagrand.png', alt: 'Casagrand', width: 110, height: 40 },
+      { src: '/partner/sis-logo.png', alt: 'SIS', width: 60, height: 20 },
+      { src: '/partner/vgn.png', alt: 'VGN', width: 90, height: 35 },
+      { src: '/partner/godrej.png', alt: 'Godrej', width: 70, height: 40 },
+      { src: '/partner/roca.png', alt: 'Roca', width: 68, height: 30 },
+      { src: '/partner/puravankara.png', alt: 'Puravankara', width: 110, height: 35 },
+      { src: '/partner/greenply.png', alt: 'Greenply', width: 60, height: 40 },
     ];
 
 
 
   return (
     <section className="relative pt-8 overflow-hidden max-h-screen">
-      <div className="px-4 sm:px-6 lg:px-28 pt-16 pb-24">
+      <div className="px-4 sm:px-8 md:px-16 lg:px-28 pt-10 md:pt-16 pb-20 md:pb-24">
         <motion.div 
-          className="relative aspect-[16/7] w-full rounded-3xl overflow-hidden "
+          className="relative aspect-[7/12] sm:aspect-[7/9] md:aspect-[16/12] lg:aspect-[16/7] w-full rounded-4xl sm:rounded-3xl overflow-hidden "
           initial={{ opacity: 0, scale: 0.95 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8 }}
@@ -91,11 +82,62 @@ const Hero = () => {
             className="object-cover"
             priority
           />
-          <div className="absolute inset-0 bg-black/10" />
+          <div className="absolute inset-0 bg-black/20 sm:bg-black/10" />
+
+          {/* mobile view */}
+            <motion.div
+              className="md:hidden absolute inset-0 flex items-center justify-center -translate-y-20 sm:-translate-y-24"
+              variants={itemVariants2}
+              initial="hidden"
+              animate="visible"
+              transition={{ delay: 0.5 }}
+            >
+              <div className="bg-white/90 backdrop-blur-sm px-4 py-3 sm:px-6 sm:py-5 rounded-2xl drop-shadow-2xl max-w-sm">
+                  <p className="text-[11px] sm:text-xs font-semibold tracking-wider text-slate-600">CHENNAIS&apos; FAVORITE INTERIORS</p>
+                <div className="flex items-center justify-center gap-2 text-yellow-500">
+                  <h2 className="text-base sm:text-lg ont-normal text-slate-900 ">
+                    4.3k+ Ratings
+                  </h2>
+                  <Star fill="currentColor" className="w-5 h-5" />
+                </div>
+              </div>
+         </motion.div>
+
+          <div className="md:hidden absolute inset-0 flex items-center justify-center text-white text-2xl sm:text-3xl text-center z-10">
+            <div>
+              <span className="inline-flex items-center gap-2">
+                Move in with a smile
+                <Image
+                  src="/smile.svg"
+                  alt="smile pic"
+                  width={36}
+                  height={36}
+                  className="inline-block"
+                />
+              </span>
+              <br />
+              Interiors That Feel Like <span className='italic'>You.</span>
+
+            </div>
+          </div>
+
+          <motion.div 
+              className="absolute bottom-2 md:hidden z-10 bg-white p-4 sm:p-6 rounded-3xl drop-shadow-2xl max-w-xl sm:max-w-2xl mx-2 border-[1px]"
+              variants={itemVariants}
+              initial="hidden"
+              animate="visible"
+          >
+              <h3 className="text-xl sm:text-2xl font-normal text-black">Make yourself at Home</h3>
+              <p className="mt-2 text-base sm:text-lg text-slate-600">Chennai&apos;s No.1 interior design — just share your floor plan and we&apos;ll handle the rest.</p>
+              <Button className="relative mt-4 mb-2 w-full bg-[#F86642] hover:bg-orange-600 text-lg sm:text-xl text-white font-medium py-7 shadow-[2px_14px_30px_-6px_#F86642]/60 rounded-xl">
+                  BOOK A FREE CONSULTATION
+              </Button>
+          </motion.div>
         </motion.div>
 
+        {/* desktop view */}
         <motion.div
-          className="absolute top-[10rem] left-[5%]"
+          className="hidden md:block absolute top-[10rem] left-[4%] lg:left-[5%] "
           variants={itemVariants2}
           initial="hidden"
           animate="visible"
@@ -104,29 +146,29 @@ const Hero = () => {
           <div className="bg-white/90 backdrop-blur-sm p-4 rounded-2xl drop-shadow-2xl max-w-sm">
             <div className="flex items-center gap-2 text-yellow-500">
               <Star fill="currentColor" className="w-5 h-5" />
-              <p className="text-xs font-semibold tracking-wider text-slate-600">CHENNAI&apos;S FAVORITE INTERIORS</p>
+              <p className="text-[11px] lg:text-xs font-semibold tracking-wider text-slate-600">CHENNAIS&apos; FAVORITE INTERIORS</p>
             </div>
-            <h2 className="text-2xl font-normal text-slate-900 mt-2">
+            <h2 className="text-lg lg:text-2xl font-normal text-slate-900 lg:mt-2">
               4.3k+ Ratings
             </h2>
           </div>
         </motion.div>
 
         <motion.div
-          className="absolute bottom-[20rem] right-[4%] sm:bottom-[10rem] lg:bottom-1/2 lg:translate-y-6"
+          className="hidden absolute inset-0 md:flex justify-end items-center -translate-x-10 lg:-translate-x-18 -translate-y-10 "
           variants={itemVariants1}
           initial="hidden"
           animate="visible"
         >
-          <div className="bg-white p-6 rounded-2xl drop-shadow-xl max-w-[420px] border-[1px]">
+          <div className="bg-white p-6 rounded-2xl drop-shadow-xl max-w-[360px] lg:max-w-[420px] border-[1px]">
               <div className="flex items-center gap-3">
-                  <Image src="/Avatar.png" alt="Aishwarya R." width={48} height={48} className="rounded-full" />
+                  <Image src="/Avatar.png" alt="Aishwarya R." width={48} height={48} className="rounded-full w-10 h-10 lg:w-12 lg:h-12" />
                   <div>
-                      <p className="font-bold text-base text-[#010101]">Aishwarya R.</p>
-                      <p className="text-sm text-[#868686]">3BHK in Anna Nagar, Chennai.</p>
+                      <p className="font-bold text-sm lg:text-base text-[#010101]">Aishwarya R.</p>
+                      <p className="text-xs lg:text-sm text-[#868686]">3BHK in Anna Nagar, Chennai.</p>
                   </div>
               </div>
-              <p className="mt-4 text-lg/[26px] text-black ">
+              <p className="mt-4 text-sm/[26px] lg:text-lg/[26px] text-black ">
                   “They really listened, understood my ideas, and turned them into a space that feels like mine.”
               </p>
               <p className="w-full flex justify-end">
@@ -135,33 +177,37 @@ const Hero = () => {
                     alt='signature'
                     width={180}
                     height={60}
-                    className='w-[180px]'
+                    className='w-40 lg:w-[180px]'
                 />
               </p>
           </div>
         </motion.div>
 
         <motion.div 
-            className="absolute translate-x-20 translate-y-4 -mt-20 z-10 bg-white p-6 rounded-2xl shadow-xl max-w-sm mx-auto"
+            className="hidden md:block absolute translate-x-6 lg:translate-x-20 translate-y-4 -mt-20 z-10 bg-white p-4 lg:p-6 rounded-2xl drop-shadow-2xl max-w-[280px] lg:max-w-sm mx-auto border-[1px]"
             variants={itemVariants}
             initial="hidden"
             animate="visible"
+            whileHover={{ scale: 1.02, boxShadow: '0px 8px 30px rgba(0, 0, 0, 0.15)' }}
+            transition={{ type: "spring", stiffness: 200, damping: 15 }}
         >
-            <h3 className="text-xl font-normal text-black">Make yourself at Home</h3>
-            <p className="mt-2 text-slate-600">Chennai&apos;s No.1 interior design — just share your floor plan and we&apos;ll handle the rest.</p>
-            <Button className="relative mt-4 w-full bg-[#F86642] hover:bg-orange-600 text-lg text-white font-medium py-6 shadow-[2px_10px_30px_-6px_#F86642]/60">
+            <h3 className="text-base lg:text-xl font-normal text-black">Make yourself at Home</h3>
+            <p className="mt-2 text-sm lg:text-base text-slate-600">Chennai&apos;s No.1 interior design — just share your floor plan and we&apos;ll handle the rest.</p>
+            <Button className="relative mt-4 w-full bg-[#F86642] hover:bg-orange-600 text-base lg:text-lg text-white font-medium py-6 shadow-[2px_10px_30px_-6px_#F86642]/60 rounded-xl cursor-pointer active:scale-102">
                 BOOK A FREE CONSULTATION
             </Button>
         </motion.div>
 
+        
+
         <motion.div 
-          className="mt-6 ml-[30%] relative overflow-hidden "
+          className="mt-4 md:mt-6 md:ml-[38%] lg:ml-[30%] relative overflow-hidden "
           variants={containerVariants}
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true }}
         >
-            <h4 className="text-lg font-semibold font-geist tracking-widest text-[#ABABAB] mb-6">OUR TRUSTED PARTNERS</h4>
+            <h4 className="text-base lg:text-lg font-semibold font-geist tracking-widest text-[#ABABAB] mb-4 md:mb-6 text-center md:text-start">OUR TRUSTED PARTNERS</h4>
             <div className="relative w-full overflow-hidden">
     
                 <div className="absolute left-0 top-0 h-full w-24 bg-gradient-to-r from-white to-transparent z-10 pointer-events-none" />
@@ -178,13 +224,13 @@ const Hero = () => {
                 }}
                 >
                 {[...partners, ...partners].map(({ src, alt, width, height }, i) => (
-                    <Image
-                    key={`${alt}-${i}`}
-                    src={src}
-                    alt={alt}
-                    width={width || 80}
-                    height={height || 40}
-                    className="opacity-80 grayscale hover:opacity-100 hover:grayscale-0 transition-all"
+                     <Image
+                      key={`${alt}-${i}`}
+                      src={src}
+                      alt={alt}
+                      width={width || 80}
+                      height={height || 40}
+                      className="opacity-80 grayscale hover:opacity-100 hover:grayscale-0 transition-all h-5 lg:h-8 w-auto object-contain"
                     />
                 ))}
                 </motion.div>
