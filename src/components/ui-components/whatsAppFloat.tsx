5736ef65a6172bb573acb6a91ed0c9fd2013c6d6
EpicAryan <adaryan0811@gmail.com>
2025-07-17 03:25:06 +0530

    fix: visual bug on whatsapp

--- src/components/ui-components/whatsAppFloat.tsx
+++ src/components/ui-components/whatsAppFloat.tsx
@@ -146,7 +146,6 @@ export const WhatsAppFloat: React.FC<WhatsAppFloatProps> = ({
           initial={{ x: 100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ type: "spring", stiffness: 260, damping: 20, delay: 1 }}
-          whileHover={{ x: -5 }}
           whileTap={{ scale: 0.95 }}
         >
           {/* Background container that extends to the edge */}
@@ -186,24 +185,15 @@ export const WhatsAppFloat: React.FC<WhatsAppFloatProps> = ({
                 <path fill="#FFF" d="M783.302 243.246c-69.329-69.387-161.529-107.619-259.763-107.658-202.402 0-367.133 164.668-367.214 367.072-.026 64.699 16.883 127.854 49.017 183.522l-52.096 190.229 194.665-51.047c53.636 29.244 114.022 44.656 175.482 44.682h.151c202.382 0 367.128-164.688 367.21-367.094.039-98.087-38.121-190.319-107.452-259.706zM523.544 808.047h-.125c-54.767-.021-108.483-14.729-155.344-42.529l-11.146-6.612-115.517 30.293 30.834-112.592-7.259-11.544c-30.552-48.579-46.688-104.729-46.664-162.379.066-168.229 136.985-305.096 305.339-305.096 81.521.031 158.154 31.811 215.779 89.482s89.342 134.332 89.312 215.859c-.066 168.243-136.984 305.118-305.209 305.118zm167.415-228.515c-9.177-4.591-54.286-26.782-62.697-29.843-8.41-3.062-14.526-4.592-20.645 4.592-6.115 9.182-23.699 29.843-29.053 35.964-5.352 6.122-10.704 6.888-19.879 2.296-9.176-4.591-38.74-14.277-73.786-45.526-27.275-24.319-45.691-54.359-51.043-63.543-5.352-9.183-.569-14.146 4.024-18.72 4.127-4.109 9.175-10.713 13.763-16.069 4.587-5.355 6.117-9.183 9.175-15.304 3.059-6.122 1.529-11.479-.765-16.07-2.293-4.591-20.644-49.739-28.29-68.104-7.447-17.886-15.013-15.466-20.645-15.747-5.346-.266-11.469-.322-17.585-.322s-16.057 2.295-24.467 11.478-32.113 31.374-32.113 76.521c0 45.147 32.877 88.764 37.465 94.885 4.588 6.122 64.699 98.771 156.741 138.502 21.892 9.45 38.982 15.094 52.308 19.322 21.98 6.979 41.982 5.995 57.793 3.634 17.628-2.633 54.284-22.189 61.932-43.615 7.646-21.427 7.646-39.791 5.352-43.617-2.294-3.826-8.41-6.122-17.585-10.714z"></path>
               </motion.svg>
             </motion.div>
-            
-            {/* Notification dot */}
-            {/* <motion.div 
-              className="absolute -top-1 -left-1 size-2 sm:size-3 bg-red-500 rounded-full border-2 border-white shadow-lg"
-              animate={{ scale: [1, 1.2, 1] }}
-              transition={{ duration: 1.5, repeat: Infinity }}
-            /> */}
           </div>
           
-          {/* Tooltip - positioned to the left since we're on the right edge */}
-          <motion.div 
+          {/* Tooltip - shows on hover of whole component */}
+          <div 
             className="absolute bottom-full right-2 mb-2 px-3 py-2 bg-gray-900/90 backdrop-blur-sm text-white text-xs sm:text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-all duration-300 whitespace-nowrap"
-            initial={{ y: 10, opacity: 0 }}
-            whileHover={{ y: 0, opacity: 1 }}
           >
             Connect with us on WhatsApp
             <div className="absolute top-full right-4 w-0 h-0 border-l-4 border-l-transparent border-r-4 border-r-transparent border-t-4 border-t-gray-900/90"></div>
-          </motion.div>
+          </div>
         </motion.div>
       </DialogTrigger>
 
